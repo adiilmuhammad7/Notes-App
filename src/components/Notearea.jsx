9d032397fc4b559955ddcfd395d96f1c41be07b4
Adil <adil@Adils-MacBook-Air.local>
2026-09-16 12:55:36 +0530

    Added the delete button

--- src/components/Notearea.jsx
+++ src/components/Notearea.jsx
@@ -39,58 +39,74 @@ function Notearea() {
     }
 
 
+    //delete
+    function deleteNote(index) {
+        setNotes(notes.filter(function (note, i) {
+            return i !== index
+        }))
+    }
+
+
 
 
 
     return (
-        <div className="border border-gray-700 w-150  mx-auto my-8 h-100 bg-blue-100">
-            <div className="mt-10- px-10 py-5">
-                <input
-                    value={title}
-                    onChange={(event) =>
-                        setTitle(event.target.value)
-                    }
-                    className="w-full h-15 px-5 border border-gray-400"
-                    type="text"
-                    placeholder="Title" />
-                <input
-                    value={content}
-                    onChange={(event) =>
-                        setContent(event.target.value)
-                    }
+        <div>
+            <div className="border border-gray-700 w-150  mx-auto my-8 h-100 bg-blue-100">
+                <div className="mt-10- px-10 py-5">
+                    <input
+                        value={title}
+                        onChange={(event) =>
+                            setTitle(event.target.value)
+                        }
+                        className="w-full h-15 px-5 border border-gray-400"
+                        type="text"
+                        placeholder="Title" />
+                    <input
+                        value={content}
+                        onChange={(event) =>
+                            setContent(event.target.value)
+                        }
 
-                    className="w-full h-48 px-2 border border-gray-400 mt-8 "
-                    type="text"
-                    placeholder="Enter the content"
-                />
-                <button
-                    onClick={addNote}
-                    className="mx-40 bg-amber-700 rounded w-45 h-8 mt-8">
-                    Add Note
-                </button>
+                        className="w-full h-48 px-2 border border-gray-400 mt-8 "
+                        type="text"
+                        placeholder="Enter the content"
+                    />
+                    <button
+                        onClick={addNote}
+                        className="mx-40 bg-amber-700 rounded w-45 h-8 mt-8">
+                        Add Note
+                    </button>
+                </div>
+                <div className="mx-auto">
+
+                </div>
             </div>
-            <div className="mx-auto">
-
-            </div>
-
-
-
             {/* area of input displaying under the form */}
 
-            <div className="bg-gray-100 mt-10 p-6">
+            <div className="grid grid-cols-3 gap-1">
 
-                {notes.map((note) => {
+                {notes.map((note,index) => {
                     return (
-                        <div className="bg-amber-300 p-4 mb-4 h-50">
+
+                        <div className="bg-amber-300 rounded-2xl p-4 mb-4 h-90 ml-10 mr-10">
+
                             <h2 className="font-bold text-xl mt-5 ml-6">{note.title}</h2>
                             <p className="text-lg mt-2 ml-6">{note.content}</p>
+
+                            <button 
+                            className="bg-red-600 w-20 mt-50 ml-6 rounded-2xl"
+                            onClick={function () {
+                                deleteNote(index)
+                            }}>
+                                Delete
+                            </button>
                         </div>
                     )
                 })}
 
             </div>
         </div>
-
 
     )
 }
